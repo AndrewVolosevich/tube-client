e6a568ee50452cd7f1b0892600afa99edb9d76f3
AndrewVolosevich <volosevich1989@gmail.com>
2021-02-15 10:06:51 +0300

    feat: add formik

--- src/UI/text-input/index.tsx
+++ src/UI/text-input/index.tsx
@@ -2,9 +2,13 @@ import React, { ChangeEvent } from "react";
 import styles from "./text-input.module.scss";
 
 const TextInput = (props: {
+  name: string;
   label: string;
-  value: string;
+  value: string | undefined;
   onChange: (
+    event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>
+  ) => void;
+  onBlur: (
     event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>
   ) => void;
   type?: "text" | "password" | "email" | "tel" | "area" | "number";
@@ -15,9 +19,11 @@ const TextInput = (props: {
         <div className={styles.group}>
           <textarea
             required
+            className={styles.scrollBar}
+            name={props.name}
             value={props.value}
             onChange={props.onChange}
-            className={styles.scrollBar}
+            onBlur={props.onBlur}
           />
           <span className={styles.highlight} />
           <span className={styles.bar} />
@@ -27,9 +33,11 @@ const TextInput = (props: {
         <div className={styles.group}>
           <input
             required
+            name={props.name}
             type={props.type || "text"}
             value={props.value}
             onChange={props.onChange}
+            onBlur={props.onBlur}
           />
           <span className={styles.highlight} />
           <span className={styles.bar} />
--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useInput } from "~/hooks/useInput";
 import Layout from "~/components/layout";
 import Container from "~/UI/containers/container";
 import PageTitle from "~/components/page-title/page-title";
@@ -13,6 +12,14 @@ import api from "~/api/api";
 import { useDispatch } from "react-redux";
 import { setUser } from "~/stores/user/actions";
 import { User } from "~/interfaces/api-types";
+import { Formik, FormikHelpers } from "formik";
+
+interface AuthFields {
+  username?: string;
+  email: string;
+  password: string;
+  repeatPassword?: string;
+}
 
 const AuthPage = () => {
   const router = useRouter();
@@ -25,10 +32,10 @@ const AuthPage = () => {
     }
   }, [router.query]);
 
-  const nameInput = useInput("");
-  const emailInput = useInput("");
-  const passwordInput = useInput("");
-  const comparePasswordInput = useInput("");
+  const initialFields =
+    authType === "signIn"
+      ? { email: "", password: "" }
+      : { username: "", email: "", password: "", repeatPassword: "" };
 
   const dispatch = useDispatch();
   const saveUser = (data: User) => {
@@ -67,74 +74,147 @@ const AuthPage = () => {
             console.error(err);
           });
   };
+
+  const validateHandler = (values: AuthFields) => {
+    const errors: AuthFields = initialFields;
+
+    if (authType === "signUp") {
+      if (!values.email) {
+        errors.email = "Поле обязательно для заполнения";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+      ) {
+        errors.email = "Проверьте вводимую почту";
+      } else if (!values.password) {
+        errors.password = "Поле обязательно для заполнения";
+      } else if (values.password.length < 6) {
+        errors.password = "Длина пароля должна быть не менее 6 символов";
+      } else if (!values.username) {
+        errors.username = "Поле обязательно для заполнения";
+      } else if (!(values.repeatPassword === values.password)) {
+        errors.repeatPassword = "Пароли должны совпадать";
+      }
+      return errors;
+    } else if (authType === "signIn") {
+      if (!values.email) {
+        errors.email = "Поле обязательно для заполнения";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+      ) {
+        errors.email = "Проверьте вводимую почту";
+      } else if (!values.password) {
+        errors.password = "Поле обязательно для заполнения";
+      } else if (values.password.length < 6) {
+        errors.password = "Длина пароля должна быть не менее 6 символов";
+      }
+      return errors;
+    }
+  };
+
   return (
     <Layout>
       <PageTitle title={authType === "signUp" ? "Регистрация" : "Вход"} />
       <Container>
-        <form
-          className={styles.auth_form}
-          onSubmit={(e) => {
-            e.preventDefault();
-            submitHandler();
+        <Formik
+          initialValues={initialFields}
+          validate={validateHandler}
+          onSubmit={(values, { setSubmitting }: FormikHelpers<AuthFields>) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
           }}
         >
-          <div className={styles.img_wrapper}>
-            <LockImg />
-          </div>
-          {authType === "signUp" ? (
-            <>
-              <TextInput
-                label={`Имя`}
-                value={nameInput.value}
-                onChange={nameInput.onChange}
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isValidating,
+            isSubmitting,
+          }) => (
+            <form className={styles.auth_form} onSubmit={handleSubmit}>
+              <div className={styles.img_wrapper}>
+                <LockImg />
+              </div>
+              {authType === "signUp" ? (
+                <>
+                  <TextInput
+                    label={`Имя`}
+                    name="username"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.username}
+                  />
+                  <TextInput
+                    label={`Почта`}
+                    name="email"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.email}
+                  />
+                  {errors.email && touched.email && errors.email}
+                  <TextInput
+                    label={`Пароль`}
+                    name="password"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.password}
+                  />
+                  <TextInput
+                    label={`Повторите пароль`}
+                    name="repeatPassword"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.repeatPassword}
+                  />
+                </>
+              ) : (
+                <>
+                  <TextInput
+                    label={`Почта`}
+                    name="email"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.email}
+                  />
+                  <TextInput
+                    label={`Пароль`}
+                    name="password"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.password}
+                  />
+                </>
+              )}
+              <Button
+                // disabled={!isSubmitting}
+                type={"submit"}
+                addedClasses={styles.btn}
+                size={"wide"}
+                color={"secondary"}
+                fashion={"fill"}
+                text={authType === "signUp" ? "Зарегистрироваться" : "Войти"}
+                onClick={() => {
+                  console.log(errors, touched);
+                  console.log(values);
+                  console.log(isValidating, isSubmitting);
+                }}
               />
-              <TextInput
-                label={`Email`}
-                value={emailInput.value}
-                onChange={emailInput.onChange}
-              />
-              <TextInput
-                label={`Пароль`}
-                value={passwordInput.value}
-                onChange={passwordInput.onChange}
-              />
-              <TextInput
-                label={`Повторите пароль`}
-                value={comparePasswordInput.value}
-                onChange={comparePasswordInput.onChange}
-              />
-            </>
-          ) : (
-            <>
-              <TextInput
-                label={`Email`}
-                value={emailInput.value}
-                onChange={emailInput.onChange}
-              />
-              <TextInput
-                label={`Пароль`}
-                value={passwordInput.value}
-                onChange={passwordInput.onChange}
-              />
-            </>
+              <Link
+                href={authType === "signUp" ? "/auth?signIn" : "/auth?signUp"}
+              >
+                <a className={styles.link}>
+                  {authType === "signUp"
+                    ? "Уже зарегистрированы? Войти."
+                    : "Нет аккаунта? Зарегистрируйтесь."}
+                </a>
+              </Link>
+            </form>
           )}
-          <Button
-            // disabled={true}
-            type={"submit"}
-            addedClasses={styles.btn}
-            size={"wide"}
-            color={"secondary"}
-            fashion={"fill"}
-            text={authType === "signUp" ? "Зарегистрироваться" : "Войти"}
-          />
-          <Link href={authType === "signUp" ? "/auth?signIn" : "/auth?signUp"}>
-            <a className={styles.link}>
-              {authType === "signUp"
-                ? "Уже зарегистрированы? Войти."
-                : "Нет аккаунта? Зарегистрируйтесь."}
-            </a>
-          </Link>
-        </form>
+        </Formik>
       </Container>
       <br />
     </Layout>
